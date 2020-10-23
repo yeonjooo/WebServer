9a91afd3c1f8d466eb644729c6b861791dd6f659
yeonjooo <yeonjooo13@gmail.com>
2020-10-23 23:50:32 +0900

    [CHG] Set define value and add socket options.

--- main.cpp
+++ main.cpp
@@ -2,36 +2,46 @@
 #ifdef WIN32
 #include <WinSock2.h>
 #else
+#include <netinet/in.h>
 #include <sys/socket.h>
 #endif
-int main()
+
+#define BACKLOGSIZE 5
+#define BUFSIZE 30
+#define PORTNUM 8080
+
+int main(int argc, char **argv)
 {
-    std::cout<<"Start Server Process!"<<std::endl;
+    std::cout<<"Start Server Process!" << std::endl;
+
 #ifdef WIN32
     WSADATA wsadata;
     WSAStartup(MAKEWORD(2,2), &wsadata);
 #endif
+
     SOCKET server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    SOCKADDR_IN addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(8081);
-    if (bind(server_sock, (SOCKADDR*)&addr, sizeof(SOCKADDR_IN)) < 0) {
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, 0, 0);
+
+    SOCKADDR_IN server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(PORTNUM);
+    if (bind(server_sock, (SOCKADDR*)&server_addr, sizeof(SOCKADDR_IN)) < 0) {
         std::cout << "Error while binding.." << std::endl;
         return -1;
     }
-    listen(server_sock, 5);
+    listen(server_sock, BACKLOGSIZE);
 
     SOCKADDR_IN clientAddr;
     int len;
-    SOCKET cli_sock = accept(server_sock, (SOCKADDR*)&clientAddr, &len);
+    SOCKET client_sock = accept(server_sock, (SOCKADDR*)&clientAddr, &len);
 
-    char buf[30];
+    char buf[BUFSIZE];
     memset(buf, 0, sizeof(buf));
-    int n = recv(cli_sock, buf, 29, 0);
+    int n = recv(client_sock, buf, BUFSIZE - 1, 0);
+    std::cout << "read buf : " << buf << "(" << n << ")" << std::endl;
 
-    std::cout << "read buf : " << buf << "(" << n << ")" <<std::endl;
     WSACleanup();
     return 0;
 }
